26b80f87414da4c073f3061f358c8231a4445de7
Shrijul-Venkatesh <venkateshshrijul@gmail.com>
2025-09-21 22:06:59 +0530

    Folder restructure

--- ai_art_education/agents/sequential_workflow/agent.py
+++ ai_art_education/agents/sequential_workflow/agent.py
@@ -1,13 +1,13 @@
 from google.adk.agents import SequentialAgent
-from agents.lesson_generator_agent import lesson_agent
-from agents.tutor_agent import tutor_agent
+from ai_art_education.agents.lesson_generator_agent import lesson_agent
+from ai_art_education.agents.tutor_agent import tutor_agent
 
 # Define main pipeline agent
 main_pipeline = SequentialAgent(
     name="art_education_pipeline",
     sub_agents=[
         # Step 1: Generate raw lesson
-        lesson_agent,  
+        lesson_agent,
 
         # Step 2: Tutor agent refines / compares using lesson data
         tutor_agent
